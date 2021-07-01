5704e9ce1dc1b0e7b397a2d035dec4b7bc34a46f
CyberSoc <cyber@yusu.org>
2021-07-01 15:01:47 +0000

    deployed via Gatsby Publish Action 🎩 for 4ad9011ec39a19aab35a5c519c6024f821439a71

---
+++ component---src-pages-redirect-js-c7ae184d8fb222572972.js
@@ -0,0 +1,2 @@
+(self.webpackChunkcybersocyork_github_io=self.webpackChunkcybersocyork_github_io||[]).push([[342],{4133:function(u,c,e){"use strict";e.r(c);var n=e(7294),t=e(5444);c.default=function(){return(0,n.useEffect)((function(){(0,t.c4)("/")}),[]),null}}}]);
+//# sourceMappingURL=component---src-pages-redirect-js-c7ae184d8fb222572972.js.map
